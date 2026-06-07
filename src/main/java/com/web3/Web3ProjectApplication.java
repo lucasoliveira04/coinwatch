8387796ad7ff7a7cced096e70678932a42ea9a37
Lucas Oliveira <camposdlucasoli@gmail.com>
2026-06-07 12:16:56 -0300

    Init

---
+++ src/main/java/com/web3/Web3ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.web3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web3ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web3ProjectApplication.class, args);
+    }
+
+}
